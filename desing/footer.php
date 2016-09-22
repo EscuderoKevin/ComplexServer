2f0f6ade680dde632c16270af40fe04c8b935fd9
EscuderoKevin <dnger.cs@hotmail.com>
2016-09-22 01:13:41 -0300

    Add files via upload

---
+++ desing/footer.php
@@ -0,0 +1,30 @@
+<footer class="main-footer">
+	<div class="pull-right hidden-xs">
+		<b>Version</b> 1.0
+	</div>
+	<strong>Copyright &copy; 2016 Theme: <a href="http://almsaeedstudio.com">Almsaeed Studio</a> Developer <a href="https://r4p3.net/members/escuderokevin.213/">EscuderoKevin</a> & R4P3.NET .</strong> All rights	reserved.
+</footer>
+</div>
+
+<link rel="stylesheet" type="text/css" href="/complexserver/functions/tsstatus/tsstatus.css" />
+<script type="text/javascript" src="/complexserver/functions/tsstatus/tsstatus.js"></script>
+<script src="/complexserver/desing/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script src="/complexserver/desing/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="/complexserver/desing/plugins/morris/morris.min.js"></script>
+<script src="/complexserver/desing/plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="/complexserver/desing/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="/complexserver/desing/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="/complexserver/desing/plugins/knob/jquery.knob.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+<script src="/complexserver/desing/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="/complexserver/desing/plugins/datepicker/bootstrap-datepicker.js"></script>
+<script src="/complexserver/desing/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="/complexserver/desing/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="/complexserver/desing/plugins/fastclick/fastclick.js"></script>
+<script src="/complexserver/desing/dist/js/app.min.js"></script>
+<script src="/complexserver/desing/dist/js/pages/dashboard.js"></script>
+<script src="/complexserver/desing/dist/js/demo.js"></script>
+</body>
+</html>	
